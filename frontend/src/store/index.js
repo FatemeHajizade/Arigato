f5306fdf3bee1882338242badd343b3a3fa1ea3f
Fateme Hajizade <fatemehajizade@ymail.com>
2020-01-18 18:37:50 +0330

    change the state

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,7 +4,11 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    projects: [],
+    freelancers: [],
+    categories: []
+  },
   mutations: {},
   actions: {},
   modules: {}

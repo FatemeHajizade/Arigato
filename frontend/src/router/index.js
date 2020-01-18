814599e74581b2ebbf6b5a821ef85b40b60ceac8
Fateme Hajizade <fatemehajizade@ymail.com>
2020-01-18 18:26:20 +0330

    add component to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "../views/Home";
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home
+  }
+];
 
 const router = new VueRouter({
   mode: "history",

58404496ba3f58428ecf2cdc060f0bff229a39ba
Ahmadjuni <ahmadsparof@gmail.com>
2020-01-19 21:19:39 +0330

    database init

---
+++ backend/backend/database/database.js
@@ -0,0 +1,20 @@
+let Sequelize = require('sequelize');
+
+let db = new Sequelize(
+    'postgres',
+    'postgres',
+    '2240227516',
+    {
+        host: 'localhost',
+        dialect: 'postgres',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
+        logging: false,
+        force: true
+    }
+);
+
+module.exports = db;

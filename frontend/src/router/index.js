e63b6acb512f4d6ec564c631bfbc79c1fb541188
Fateme Hajizade <fatemehajizade@ymail.com>
2020-01-19 21:00:00 +0330

    add component to router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home";
+import EditProfile from "../views/EditProfile";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "home",
     component: Home
+  },
+  {
+    path: "/profile/edit",
+    name: "editprofile",
+    component: EditProfile
   }
 ];
 

2ecbcf778d4e2c1b60954dddac68e11df879023e
Ahmadjuni <ahmadsparof@gmail.com>
2020-01-21 01:27:53 +0330

    create project function

--- backend/backend/controllers/project.controller.js
+++ backend/backend/controllers/project.controller.js
@@ -4,3 +4,43 @@ let User = require('../models/User');
 let Offer = require('../models/Offer');
 let Skill = require('../models/Skill');
 let Category = require('../models/Category');
+
+async function createProject(req, res) {
+    try{
+    let id = req.user.id;
+    Project.create({
+        projectname: req.body.projectname,
+        budget : req.body.budget,
+        description : req.body.description,
+        clientId : id
+    })
+    .then(project =>{
+        if((project)){
+            for(let skilll of req.body.skills){
+                Skill.findOne({where:{name:skilll.name}})
+                .then(skil => {
+                    return project.addSkill(skil)
+                })
+                .catch(err => console.log(err));
+            }
+        }return project;
+    })
+    .then(project =>{
+        if((project)){
+            for(let categ of req.body.categories){
+                Category.findOne({where:{name:categ.name}})
+                .then(catego => {
+                    return project.addCategories(catego)
+                })
+                .catch(err => console.log(err));
+            }
+        }return res.send(project);
+    })
+} catch(e){
+    console.log(e);
+    res.status(500).json({
+        message: 'Something goes wrong',
+        data: {}
+    });
+    }
+}

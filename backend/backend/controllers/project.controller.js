dcca1f1895f42f7c9c74f6efd4147c37b14c7752
Ahmadjuni <ahmadsparof@gmail.com>
2020-01-21 01:25:44 +0330

    project controller adding

---
+++ backend/backend/controllers/project.controller.js
@@ -0,0 +1,6 @@
+let Sequelize = require('sequelize');
+let Project = require('../models/Project');
+let User = require('../models/User');
+let Offer = require('../models/Offer');
+let Skill = require('../models/Skill');
+let Category = require('../models/Category');

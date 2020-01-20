ee4106ff16dd3e45f9a530e2ea87c56ea24439e0
Ahmadjuni <ahmadsparof@gmail.com>
2020-01-21 00:15:04 +0330

    server and db running

---
+++ backend/backend/index.js
@@ -0,0 +1,28 @@
+let app = require('./app');
+let db = require('./database/database');
+let User = require('./models/User');
+let Skill = require('./models/Skill');
+let Category = require('./models/Category');
+let project = require('./models/Project');
+
+
+async function main(){
+    try{
+    app.listen(3500);
+    console.log('Server on port 3500');}
+    catch(err){
+        console.log(err);
+    }
+};
+main();
+
+// db.sync()
+//     .then( () => {
+//         app.listen(2854, () => {
+//             console.log('Server started on http://localhost:27017/')
+//         })
+        
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
